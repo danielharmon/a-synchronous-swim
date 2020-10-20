56314d093a8e081727d9f6ad5a37ab1db815bcb7
danielharmon <dannyaharmon@gmail.com>
2020-10-19 19:51:51 -0700

    Complete impletmenting swim command

--- server/spec/httpHandler.spec.js
+++ server/spec/httpHandler.spec.js
@@ -23,7 +23,14 @@ describe('server responses', () => {
 
   it('should respond to a GET request for a swim command', (done) => {
     // write your test here
-    done();
+    let {req, res} = server.mock('/', 'GET');
+
+    httpHandler.router(req, res, () => {
+      expect(res._responseCode).to.equal(200);
+      expect(res._ended).to.equal(true);
+      expect(res._data.toString()).to.be.oneOf(['up', 'down', 'left', 'right']);
+      done();
+    });
   });
 
   xit('should respond with 404 to a GET request for a missing background image', (done) => {
