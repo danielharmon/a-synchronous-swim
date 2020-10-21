1d965adc6f0fd2fc46906413905bc2ab51d480f6
danielharmon <dannyaharmon@gmail.com>
2020-10-20 19:31:01 -0700

    Complete Refactoring Server

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const headers = require('./cors');
 const multipart = require('./multipartUtils');
+const messageArray = require('./messageQueue');
 
 // Path for the background image ///////////////////////
 module.exports.backgroundImageFile = path.join('.', 'background.jpg');
@@ -11,12 +12,13 @@ let messageQueue = null;
 module.exports.initialize = (queue) => {
   messageQueue = queue;
 };
+
 const validInputs = ['up', 'down', 'left', 'right'];
 module.exports.router = (req, res, next = ()=>{}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
   res.writeHead(200, headers);
   if (req.method === 'GET') {
-    res.end(validInputs[Math.floor(Math.random() * validInputs.length)]);
+    res.end(messageArray.dequeue());
   }
   next(); // invoke next() at the end of a request to help with testing!
 };
