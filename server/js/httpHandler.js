2c5fde70849e3cd05bcbd8223b5061aed0ef5a80
danielharmon <dannyaharmon@gmail.com>
2020-10-21 19:30:27 -0700

    Complete Background image functionality

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -17,19 +17,50 @@ module.exports.initialize = (queue) => {
 module.exports.router = (req, res, next = ()=>{}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
 //look at path
-fs.access(req.url, function(error) {
-  if (error) {
-    res.writeHead(404, headers);
-    res.end();
-    console.log('file not found');
-  } else {
+  if (req.method === 'OPTIONS') {
     res.writeHead(200, headers);
-    if (req.method === 'GET') {
+    res.end();
+    next()
+  }
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      res.writeHead(200, headers);
       res.end(messageQueue.dequeue());
-    } else {
-      res.end();
+      next()
     }
-    console.log('file found');
-});
-  next(); // invoke next() at the end of a request to help with testing!
+    if (req.url === '/background.jpg') {
+
+      fs.readFile(module.exports.backgroundImageFile, (error, data) => {
+        if (error) {
+          console.log('error')
+          res.writeHead(404, headers);
+          res.end();
+          next()
+        } else {
+          res.writeHead(200, headers);
+          res.end(data);
+          next()
+        }
+
+      })
+    }
+  }
+
 };
+
+  // fs.access(req.url, fs.constants.F_OK, function (error) {
+  //   if (error) {
+  //     res.writeHead(404, headers);
+  //     res.end();
+  //     console.log('file not found');
+  //   } else {
+  //     res.writeHead(200, headers);
+  //     if (req.method === 'GET') {
+  //       res.end(messageQueue.dequeue());
+  //     } else {
+  //       res.end();
+  //     }
+  //   }
+  //   console.log('file found');
+
+  // invoke next() at the end of a request to help with testing!
--- server/spec/httpHandler.spec.js
+++ server/spec/httpHandler.spec.js
@@ -41,7 +41,7 @@ describe('server responses', () => {
 
   it('should respond with 404 to a GET request for a missing background image', (done) => {
     httpHandler.backgroundImageFile = path.join('.', 'spec', 'missing.jpg');
-    let {req, res} = server.mock(httpHandler.backgroundImageFile, 'GET');
+    let {req, res} = server.mock('/background.jpg', 'GET');
 
     httpHandler.router(req, res, () => {
       expect(res._responseCode).to.equal(404);
@@ -52,7 +52,8 @@ describe('server responses', () => {
 
   it('should respond with 200 to a GET request for a present background image', (done) => {
     // write your test here
-    let {req, res} = server.mock(httpHandler.backgroundImageFile, 'GET');
+    httpHandler.backgroundImageFile = path.join('.', 'background.jpg');
+    let {req, res} = server.mock('/background.jpg', 'GET');
 
     httpHandler.router(req, res, () => {
       expect(res._responseCode).to.equal(200);
